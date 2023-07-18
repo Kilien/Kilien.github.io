adc4597156fc45ec9180e3fc404d627e12e9a72a
Kilien <Kilien@users.noreply.github.com>
2023-07-18 10:46:13 +0000

    deploy: 127001bc7f1d993a706a1c751e13888fbe328660

--- assets/index.html.3f863fcf.js
+++
@@ -1,8 +0,0 @@
-import{_ as s,o as n,c as r,d as e}from"./app.9673dcd9.js";const o={},t=e(`<p>\u672C\u8D28\u4E0A\u8FD8\u662F\u6E90\u7684\u95EE\u9898\uFF1A\u9700\u8981\u7ED9electron\u8BBE\u7F6E\u4E00\u4E2A\u7279\u5B9A\u7684\u6E90\u3002</p><p>\u9879\u76EE\u4E0B\u65B0\u589E\u6587\u4EF6\uFF1A <strong>.yarnrc</strong></p><p>\u8F93\u5165\u4EE5\u4E0B\u6E90\uFF1A</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>registry <span class="token string">&quot;https://registry.npmmirror.com&quot;</span>
-sass_binary_site <span class="token string">&quot;https://npmmirror.com/mirrors/node-sass/&quot;</span>
-phantomjs_cdnurl <span class="token string">&quot;http://cnpmjs.org/downloads&quot;</span>
-electron_mirror <span class="token string">&quot;https://npmmirror.com/mirrors/electron/&quot;</span>
-sqlite3_binary_host_mirror <span class="token string">&quot;https://foxgis.oss-cn-shanghai.aliyuncs.com/&quot;</span>
-profiler_binary_host_mirror <span class="token string">&quot;https://npmmirror.com/mirrors/node-inspector/&quot;</span>
-chromedriver_cdnurl <span class="token string">&quot;https://npmmirror.com/mirrors/chromedriver/&quot;</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),i=[t];function a(c,l){return n(),r("div",null,i)}var p=s(o,[["render",a],["__file","index.html.vue"]]);export{p as default};
